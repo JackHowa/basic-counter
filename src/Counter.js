177d752d82eb5e08bfa94fa98f8e964f7d9e9ea9
Jack Howard <jackhowaa@gmail.com>
2019-10-26 06:48:06 -0500

    Simple counter with class and state

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  state = {
+    count: 0
+  }
+  
+  incrementCounter = (amount) => this.setState(prevState => ({
+    count: prevState.count + amount
+  }));
+
+  hardSetCounter = (newCount) => this.setState({
+    count: newCount
+  })
+
   render() {
+    const { count } = this.state;
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>Count: {count}</h1>
+        <button onClick={() => this.incrementCounter(1)} className="full-width">Increment</button>
+        <button onClick={() => this.incrementCounter(-1)} className="full-width">Decrement</button>
+        <button onClick={() => this.hardSetCounter(0)} className="full-width">Reset</button>
       </section>
     );
   }
